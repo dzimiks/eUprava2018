b6a6a7f99cf18983ed3930d625054626b2ecc71c
unknown <brankofulurija@gmail.com>
2018-05-27 01:04:21 +0200

    Switching tabs in navbar active class

--- euprava_server/controllers/mainController.js
+++ euprava_server/controllers/mainController.js
@@ -1,35 +1,39 @@
 module.exports.home = function (req, res, next) {
-    res.render('home', {
-        meta: {
-            title: 'eUprava | Home',
-            description: 'eUprava Home Page'
-        }
-    });
+  res.render('home', {
+    meta: {
+      title: 'eUprava | Home',
+      description: 'eUprava Home Page',
+      pocetna: 'pocetna'
+    }
+  });
 };
 
 module.exports.services = function (req, res, next) {
   res.render('services', {
     meta: {
       title: 'eUprava | Services',
-      description: 'eUprava Services Page'
+      description: 'eUprava Services Page',
+      usluge: 'usluge'
     }
   });
 };
 
 module.exports.profile = function (req, res, next) {
-    res.render('profile', {
-        meta: {
-            title: 'eUprava | Profile',
-            description: 'eUprava Profile Page'
-        }
-    });
+  res.render('profile', {
+    meta: {
+      title: 'eUprava | Profile',
+      description: 'eUprava Profile Page',
+      profil: 'profil'
+    }
+  });
 };
 
 module.exports.login = function (req, res, next) {
   res.render('login', {
     meta: {
       title: 'eUprava | Login',
-      description: 'eUprava Login Page'
+      description: 'eUprava Login Page',
+      prijava: 'prijava'
     }
   });
 };
